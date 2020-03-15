28a38cca92c57ab5e3bd9ad92873014bb9fb1523
breeze <819974025@qq.com>
2020-03-15 15:56:56 +0800

    breeze：子模块Payment - 支付模块的搭建

---
+++ cloud-provider-payment/src/main/java/com/breeze/springcloud/PaymentApplication.java
@@ -0,0 +1,15 @@
+package com.breeze.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author breeze
+ * @date 2020/3/15
+ */
+@SpringBootApplication
+public class PaymentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication.class, args);
+    }
+}
